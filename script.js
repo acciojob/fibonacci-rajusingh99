88de232e71775e80961e465b784b12c0e5626bd9
Raju Singh <94919614+rajusingh99@users.noreply.github.com>
2023-07-12 08:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function fibonacci(num) {
 // your code here
-	return num;
+	 if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let fibArr = [0, 1]; // Initialize the Fibonacci sequence with the first two terms
+    for (let i = 2; i <= num; i++) {
+      fibArr[i] = fibArr[i - 1] + fibArr[i - 2]; // Calculate the next Fibonacci number
+    }
+    return fibArr[num];
+  }
 }
 
 module.exports = fibonacci;
